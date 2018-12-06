141905d32458c684cb4984110f20562b6d944f56
katarzynakonder <45471495+katarzynakonder@users.noreply.github.com>
2018-12-07 00:32:25 +0100

    Update DockerConnectMySQL.java

--- DockerConnectMySQL.java
+++ DockerConnectMySQL.java
@@ -2,7 +2,7 @@ import java.sql.*;
 
 public class DockerConnectMySQL {
    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
-   static final String DB_URL = "jdbc:mysql://db:3306/baza;
+   static final String DB_URL = "jdbc:mysql://db:3306/baza";
    static final String USER = "kkonder";
    static final String PASS = "password";
    
