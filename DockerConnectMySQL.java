e2ad242f437eec492eb5465f733cf0aed2f36225
katarzynakonder <45471495+katarzynakonder@users.noreply.github.com>
2018-12-07 11:30:12 +0100

    Update DockerConnectMySQL.java

--- DockerConnectMySQL.java
+++ DockerConnectMySQL.java
@@ -1,107 +1,112 @@
-import java.sql.*;
-import java.util.Random;
 import java.util.Scanner;
-import java.io.Console;
+import java.sql.*;
 
 public class DockerConnectMySQL {
-   static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
-   static final String DB_URL = "jdbc:mysql://10.0.10.3:3306/baza";
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://10.0.10.3:3306/baza";
+    static final String USER = "kkonder";
+    static final String PASS = "password";
 
-   static final String USER = "kkonder";
-   static final String PASS = "password";
-   
-   public static void main(String[] args) {
-   Connection conn = null;
-   Statement stmt = null;	 
-   try{   
-         Class.forName("com.mysql.jdbc.Driver");
-	 Boolean connect = false;
-      while(!connect)
-      {
-         try {
-            conn = DriverManager.getConnection(DB_URL,USER,PASS);
-            connect = true;
-         }
-         catch(Exception e) {
-       	 
-            System.out.println("Łączenie z bazą");
-            Thread.sleep(5000);
-         }
-      }
-      stmt = conn.createStatement();
-      String sqlSelect, sqlNewDb, sqlDrop;
-	  Boolean stop = false;
-	  sqlDrop ="DROP TABLE IF EXISTS zadanko";
-      stmt.executeUpdate(sqlDrop); 
-	  sqlNewDb="CREATE TABLE zadanko ( imie varchar(255), nazwisko varchar(255), wiek int)"; 
-      System.out.println("Tworzenie bazy");
-      stmt.executeUpdate(sqlNewDb);
-      
-      stmt.executeUpdate("INSERT INTO zadanko ( imie, nazwisko, wiek)"+ 
-			 "VALUES('Michał', 'Wiśniewski', '25')"+
-			       ",('Kamil', 'Grosicki','53')"+
-			       ",('Andrzej','Grabowski', '36')");  			   
-	Scanner input = new Scanner(System.in);
-	   while(!stop)
-	   {	
-      		System.out.println("[1] Pokaż tabelę");
-      		System.out.println("[2] Dodaj encję");
-      		System.out.println("[3] Koniec"); 
-		   int val = input.nextInt();  
-		   switch(val) {
-			    case 1:
-				
-		   		ResultSet rs = stmt.executeQuery("SELECT imie, nazwisko, wiek FROM zadanko");
-
-      				while(rs.next()){
-         				String imie = rs.getString("imie");
-         				String nazwisko = rs.getString("nazwisko");
-						int wiek = rs.getInt("wiek");
-         				System.out.println("imie: " + imie + ", nazwisko:  " + nazwisko+", wiek: " + wiek);
-      				}
-				   rs.close();
-		   		break;
-				case 2:
-				
-					String imie, nazwisko;
-				        int wiek;
-					System.out.println("Podaj imię:");
-					imie=input.next();
-					System.out.println("Podaj nazwisko:");
-					nazwisko=input.next();
-		
-					System.out.println("Dodaj dane do tabeli");
-					stmt.executeUpdate("INSERT INTO zadanko ( imie, nazwisko, wiek)"+ 
-									   "VALUES('" + imie + "','" + nazwisko + "','"+ wiek +"')");
-				break;
-				case 3:
-				
-					stop=true;
-					break;
-				default:
-			
-					System.out.println("Error");
-			}
-	   }
-	   
-      stmt.close();
-      conn.close();
-   }catch(SQLException se){
-      se.printStackTrace();
-   }catch(Exception e){
-      e.printStackTrace();
-   }finally{
-      try{
-         if(stmt!=null)
+    public static void main(String[] args) {
+        Connection conn = null;
+        Statement stmt = null;
+        try {
+            Boolean connect = false;
+            do {
+                try {
+                    conn = DriverManager.getConnection(DB_URL, USER, PASS);
+                    connect = true;
+                } catch (Exception e) {
+                    System.out.println("Łączenie z bazą");
+                    Thread.sleep(1000);
+                }
+            } while (!connect);
+			System.out.println("Połączono z bazą");
+            stmt = conn.createStatement();
+            String sql;
+            sql = "DROP TABLE IF EXISTS Ludzie";
+            stmt.executeUpdate(sql);
+            sql = "CREATE TABLE Ludzie (ID int, Imię varchar(255), Nazwisko varchar(255), Wiek int );";
+            stmt.executeUpdate(sql);
+            sql = "INSERT INTO Ludzie(ID, Imię, Nazwisko, Wiek) VALUES (1,'Robert', ' Lewandowski', '35'),(2, 'Kamil', 'Grosicki, '16'),(3, 'Łukasz', 'Fabiański', '25');";
+            stmt.executeUpdate(sql);
+            Scanner menu = new Scanner(System.in);
+            String i;
+            do {
+                System.out.println("");
+                System.out.println("Wybierz nr:");
+                System.out.println("[1] Dodaj encję");
+                System.out.println("[2] Pokaż zawartość bazy");
+                System.out.println("[3] Wyjdź");
+                i = (String) menu.next();
+                switch (i) {
+                    case "1": {
+                        Scanner insert = new Scanner(System.in);
+                        sql = "SELECT ID FROM Ludzie ORDER BY ID DESC LIMIT 1;";
+                        ResultSet rs = stmt.executeQuery(sql);
+                        int e = 0;
+                        if (rs.next()) {
+                            e = rs.getInt("ID");
+                        }
+                        rs.close();
+                        e++;
+                        sql = "INSERT INTO Ludzie (ID, Imię, Nazwisko, Wiek) VALUES (" + e + ",'";
+                        System.out.println("Podaj imię:");
+                        sql += insert.nextLine();
+                        sql += "', '";
+                        System.out.println("Podaj nazwisko:");
+                        sql += insert.nextLine();
+                        sql += "', '";
+                        System.out.println("Podaj wiek:");
+                        sql += insert.nextLine();
+                        sql += "');";
+                        stmt.executeUpdate(sql);
+						System.out.println("Dodano encję");
+                        break;
+                    }
+                    case "2": {
+                        sql = "SELECT ID, Imię, Nazwisko, Wiek FROM Ludzie";
+                        ResultSet rs = stmt.executeQuery(sql);
+                        System.out.printf("|%5s|%15s|%15s|%15s|\n", "ID: ", "Imię: ", "Nazwisko: ", "Wiek: ");
+                        while (rs.next()) {
+                            int id = rs.getInt("ID");
+                            String first = rs.getString("Imię");
+                            String last = rs.getString("Nazwisko");
+                            String city = rs.getString("Wiek");
+                            System.out.printf("|%4d |%14s |%14s |%14s |\n", id, imię, nazwisko, wiek);
+                        }
+                        rs.close();
+                        break;
+                    }
+                    case "3": {
+                        i = "3";
+                        break;
+                    }
+                    default: {
+						System.out.println("");
+                        System.out.println("Wybierz odpowiednią opcje!");
+                        break;
+                    }
+                }
+            } while (i != "3");
             stmt.close();
-      }catch(SQLException se2){
-      }
-      try{
-         if(conn!=null)
             conn.close();
-      }catch(SQLException se){
-         se.printStackTrace();
-      }
-   }
- }
+        } catch (SQLException se) {
+            se.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (stmt != null)
+                    stmt.close();
+            } catch (SQLException se2) {
+            }
+            try {
+                if (conn != null)
+                    conn.close();
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }
+        }
+    }
 }
